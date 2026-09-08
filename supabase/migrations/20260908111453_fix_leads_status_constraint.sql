1dad3caecea5698bf0a67eb7a2fdebec6111ef69
hassanbuild <syedhasanali548@gmail.com>
2026-09-08 16:19:00 +0500

    Fix leads status constraint

---
+++ supabase/migrations/20260908111453_fix_leads_status_constraint.sql
@@ -0,0 +1,6 @@
+alter table public.leads
+  drop constraint if exists leads_status_check;
+
+alter table public.leads
+  add constraint leads_status_check
+  check (status in ('new', 'contacted', 'closed'));
